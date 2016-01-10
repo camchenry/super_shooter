fbd7590a4af9a2a8f8743a4e11b2bfa327850022
Nuthen <nuthen@comcast.net>
2016-01-09 21:05:27 -0500

    improved restart and win screen text

--- states/gameover.lua
+++ states/gameover.lua
@@ -25,22 +25,41 @@ end
 function gameover:draw()
 	game:draw()
 	love.graphics.setColor(255, 255, 255)
-	local text = [[
-		Thanks for playing!
-	]]
-	local text2 = [[
-		That's all for now. If you enjoyed it, leave a post on the forums!
-	]]
+	local text = "Thanks for playing!"
+	local text2 = "That's all for now. If you enjoyed it, leave a post on the forums!"
+	local text3 = "< click to return to menu >"
+	local score = "FINAL SCORE: " .. game.highScore.currentScore
+	
 	love.graphics.setFont(fontLight[48])
-	love.graphics.printf(string.upper(text), 0, love.graphics.getHeight()/2-100, love.graphics.getWidth(), "center")
+	text = string.upper(text)
+	local textX, textY = love.graphics.getWidth()/2 - fontLight[48]:getWidth(text)/2, love.graphics.getHeight()/2-100
+	textX, textY = math.floor(textX), math.floor(textY)
+	love.graphics.print(string.upper(text), textX, textY)
+	
 	love.graphics.setFont(font[24])
-	love.graphics.printf(text2, 400, love.graphics.getHeight()/2, love.graphics.getWidth()-800, "center")
+	local text2X, text2Y = love.graphics.getWidth()/2 - font[24]:getWidth(text2)/2, love.graphics.getHeight()/2
+	text2X, text2Y = math.floor(text2X), math.floor(text2Y)
+	love.graphics.print(text2, text2X, text2Y)
+	
+	local text3X, text3Y = love.graphics.getWidth()/2 - font[24]:getWidth(text3)/2, love.graphics.getHeight()/2 + 50
+	text3X, text3Y = math.floor(text3X), math.floor(text3Y)
+	love.graphics.print(text3, text3X, text3Y)
+	
 	love.graphics.setColor(255, 255, 255, math.abs(255*math.sin(self.time)))
-	love.graphics.printf("< click to return to menu >", love.graphics.getWidth()/2 - font[24]:getWidth(text2)/2, love.graphics.getHeight()/2+25)
+	love.graphics.print(text3, text3X, text3Y)
 	
 	if highscoreList:scoreIsValid(game.highScore.currentScore) then
 		love.graphics.setColor(255, 225, 0)
 		local highscoreText = "You qualify for a highscore!"
-		love.graphics.print(highscoreText, love.graphics.getWidth()/2 - font[24]:getWidth(highscoreText)/2, love.graphics.getHeight()/2+100)
+		local highscoreX, highscoreY = love.graphics.getWidth()/2 - font[24]:getWidth(highscoreText)/2, love.graphics.getHeight()/2+100
+		highscoreX, highscoreY = math.floor(highscoreX), math.floor(highscoreY)
+		love.graphics.print(highscoreText, highscoreX, highscoreY)
 	end
+	
+	love.graphics.setColor(255, 255, 255)
+	
+	love.graphics.setFont(font[36])
+	local scoreX, scoreY = love.graphics.getWidth()/2 - font[36]:getWidth(score)/2, love.graphics.getHeight()/2 + 200
+	scoreX, scoreY = math.floor(scoreX), math.floor(scoreY)
+	love.graphics.print(score, scoreX, scoreY)
 end
--- states/restart.lua
+++ states/restart.lua
@@ -23,27 +23,37 @@ end
 function restart:draw()
 	game:draw()
 	love.graphics.setColor(255, 255, 255)
-	local text = [[
-		PROGRAM TERMINATED
-	]]
+	local text = "PROGRAM TERMINATED"
 	local score = "SCORE: " .. game.highScore.currentScore
 	local wave = "WAVE: " .. game.wave
-	local text2 = [[
-		< click anywhere to reboot >
-	]]
+	local text2 = "< click anywhere to reboot >"
+	
 	love.graphics.setFont(fontLight[48])
-	love.graphics.printf(string.upper(text), 0, love.graphics.getHeight()/2-200, love.graphics.getWidth(), "center")
+	local textX, textY = love.graphics.getWidth()/2 - fontLight[48]:getWidth(text)/2, love.graphics.getHeight()/2-200
+	textX, textY = math.floor(textX), math.floor(textY)
+	love.graphics.print(text, textX, textY)
+	
 	love.graphics.setFont(font[36])
-	love.graphics.printf(string.upper(score), 0, love.graphics.getHeight()/2-125, love.graphics.getWidth(), "center")
+	local scoreX, scoreY = love.graphics.getWidth()/2 - font[36]:getWidth(score)/2, love.graphics.getHeight()/2-125
+	scoreX, scoreY = math.floor(scoreX), math.floor(scoreY)
+	love.graphics.print(score, scoreX, scoreY)
+	
 	love.graphics.setFont(font[36])
-	love.graphics.printf(string.upper(wave), 0, love.graphics.getHeight()/2-70, love.graphics.getWidth(), "center")
+	local waveX, waveY =  love.graphics.getWidth()/2 - font[36]:getWidth(wave)/2, love.graphics.getHeight()/2-70
+	waveX, waveY = math.floor(waveX), math.floor(waveY)
+	love.graphics.print(wave, waveX, waveY)
+	
 	love.graphics.setFont(font[24])
+	local text2X, text2Y = love.graphics.getWidth()/2 - font[24]:getWidth(text2)/2, love.graphics.getHeight()/2+25
+	text2X, text2Y = math.floor(text2X), math.floor(text2Y)
 	love.graphics.setColor(255, 255, 255, math.abs(255*math.sin(self.time)))
-	love.graphics.print(text2, love.graphics.getWidth()/2 - font[24]:getWidth(text2)/2, love.graphics.getHeight()/2+25)
+	love.graphics.print(text2, text2X, text2Y)
 	
 	if highscoreList:scoreIsValid(game.highScore.currentScore) then
 		love.graphics.setColor(255, 225, 0)
 		local highscoreText = "You qualify for a highscore!"
-		love.graphics.print(highscoreText, love.graphics.getWidth()/2 - font[24]:getWidth(highscoreText)/2, love.graphics.getHeight()/2+100)
+		local highscoreX, highscoreY = love.graphics.getWidth()/2 - font[24]:getWidth(highscoreText)/2, love.graphics.getHeight()/2+100
+		highscoreX, highscoreY = math.floor(highscoreX), math.floor(highscoreY)
+		love.graphics.print(highscoreText, highscoreX, highscoreY)
 	end
 end
