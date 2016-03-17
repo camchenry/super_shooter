f16b0a65762e95130bf28fc8a7e6830db13d3a9d
camchenry <camchenry@users.noreply.github.com>
2016-03-16 23:03:26 -0400

    Fixed some font issues
    Also update git ignore to ignore more vim files

--- states/game.lua
+++ states/game.lua
@@ -22,9 +22,7 @@ function game:add(obj, tabl)
     end
     table.insert(tabl, obj)
 
-    local tol = self.collideTolerance
-    local dubTol = tol * 2
-    self.world:add(obj, obj.position.x - tol, obj.position.y - tol, obj.width + dubTol, obj.height + dubTol)
+    self.world:add(obj, obj.position.x, obj.position.y, obj.width, obj.height)
 
     return obj
 end
@@ -59,13 +57,11 @@ function game:init()
 
 	self.camera = Camera(0, 0)
 	self.camera.scale = self.cameraZoom
-
-    self.collideTolerance = 5 -- extra tollerance for AABB collision checking. Does not effect circular collision checking (which is more accurate)
 end
 
 function game:reset()
     options:load()
-    self.worldSize = vector(2500, 1500)
+    self.worldSize = vector(3000, 2000)
 
     objects = {}
     bullets = {}
@@ -149,16 +145,12 @@ function game:update(dt)
     end
 
     if player.health >= 0 then
-        local tol = self.collideTolerance
-        local dubTol = tol * 2
-
         local toUpdate = {objects, bullets}
         for i, tabl in ipairs(toUpdate) do
             for j, obj in ipairs(tabl) do
                 -- update object positions
                 obj:update(dt)
-
-                self.world:update(obj, obj.position.x - tol, obj.position.y - tol, obj.width + dubTol, obj.height + dubTol)
+                self.world:update(obj, obj.position.x, obj.position.y, obj.width, obj.height)
 
                 -- check for object collisions
                 local ax, ay, cols, len = self.world:check(obj, obj.position.x, obj.position.y)
@@ -272,10 +264,6 @@ function game:keypressed(key, isrepeat)
     if key == "p" or key == "escape" then
         state.switch(pause)
     end
-
-    if self.mode.keypressed then
-        self.mode:keypressed(key, isrepeat)
-    end
 end
 
 function game:mousepressed(x, y, mbutton)
@@ -363,6 +351,7 @@ function game:draw()
 
         if DEBUG then
             love.graphics.setColor(255, 255, 255, 255)
+            love.graphics.setFont(font[16])
             love.graphics.print("Memory usage: " .. math.floor(collectgarbage("count")/1000) .. "MB", 5, 25)
         end
 
@@ -408,6 +397,7 @@ function game:drawEntityInspectorInfo()
                        entityX + radius,
                        entityY + radius*2)
     local text = entity.width
+    love.graphics.setFont(font[20])
     love.graphics.print(text, entityX - f:getWidth(text)/2, entityY + radius*2 - f:getHeight(text)/2 + padding )
 
     -- height
@@ -488,16 +478,13 @@ end
 function game:drawCollisionBodies()
     love.graphics.setColor(255, 255, 255, 200)
     love.graphics.setLineWidth(1)
-    local tol = self.collideTolerance
-    local dubTol = tol * 2
-
     for i, object in ipairs(objects) do
         love.graphics.circle("line", object.position.x, object.position.y, object.radius)
-        love.graphics.rectangle("line", object.position.x - object.width/2 - tol, object.position.y - object.height/2 - tol, object.width + dubTol, object.height + dubTol)
+        love.graphics.rectangle("line", object.position.x - object.width/2, object.position.y - object.height/2, object.width, object.height)
     end
     for i, object in ipairs(bullets) do
         love.graphics.circle("line", object.position.x, object.position.y, object.radius)
-        love.graphics.rectangle("line", object.position.x - object.width/2 - tol, object.position.y - object.height/2 - tol, object.width + dubTol, object.height + dubTol)
+        love.graphics.rectangle("line", object.position.x - object.width/2, object.position.y - object.height/2, object.width, object.height)
     end
 end
 
